2ea7789395bbac9b9d6fa91d085dd6a213621d21
Sameer Yadav <sameer.yadv8@gmail.com>
2023-06-11 05:48:34 +0530

    Authentication Part successful

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ This webapp provides all mess related things at one place.
 1. Fork this repo.
 2. Make change.
 3. Make Pull request.
+
+
+## API Documentation
+
+Below are APIs take `POST` request in `x-www-form-urlencoded`.
+
+1. `/authenticate/generateOTP` : if we will send request to this then it will generate otp and send it to the user. It take `roll` as parameter. it will give two type of `{status: true}` for successful otp sent or `{status:false}` on some error.
+
+2. `/authenticate` : it will take two params `roll` and `otp`. it will give two type of response `authenticated:true` if otp is correct or `authenticated:false` if otp is not correct or otp has expired or any other server error
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const bodyParser = require('body-parser')
 const otpGenerator = require('otp-generator');
 const { sendMail } = require('./scripts/mail')
 require('dotenv').config()
@@ -17,6 +18,7 @@ if (app.get('env')==='production'){
     sess.cookie.secure = true
 }
 app.use(session(sess))
+app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT || 3000
 
 const dbUrl = "mongodb+srv://admin:"+process.env.DBPASS+"@cluster0.okpzlrj.mongodb.net/?retryWrites=true&w=majority"
@@ -28,7 +30,8 @@ async function createConnection() {
 const userSchema = new mongoose.Schema({
     name: String,
     roll: Number,
-    email: String
+    email: String,
+    otp : {data: String,time:Date} 
 })
 
 const User = mongoose.model('User',userSchema)
@@ -37,5 +40,40 @@ async function getEmail(roll) {
     const detail =  await User.findOne({roll:roll})
     return detail.email
 }
+
+app.post('/authenticate/generateOTP',async (req,res) => {
+    const roll = parseInt(req.body.roll)
+    const otp = otpGenerator.generate(6,{specialChars:false,upperCaseAlphabets:false,lowerCaseAlphabets:false})
+    const now = new Date()
+    const time = now.getTime()
+    getEmail(roll).then(async (email) => {
+        sendMail(email,otp).then( async () => {
+            console.log('OTP Send Successfully')
+            await User.findOneAndUpdate({roll:roll},{otp:{data:otp,time:time}}).catch(err => {console.log(err);res.json({'status':false});return})
+            res.json({'status':true})
+        }).catch(err => {console.log(err);res.json({'status':'error'})})
+    }).catch(err => {console.log(err);res.json({'status':'error'})})
+    return
+})
+
+app.post('/authenticate',async (req,res) => {
+    const roll = parseInt(req.body.roll)
+    const otp = req.body.otp
+    const now = new Date()
+    const time = now.getTime()
+
+    const student = await User.findOne({roll:roll})
+    const otpSent = student.otp
+    const timeDiff = (time - otpSent.time)/(1000*60)
+
+    if (otp===otpSent.data && timeDiff <= 3) {
+        res.json({'authenticated':true})
+        return
+    }
+    else {
+        res.json({'authenticated':false})
+        return
+    }
+})
 
 app.listen(port,() => console.log(`Server is running on http://localhost:${port}`))
